0285f8e8d252d31d1293867f46a6944fdd9c557e
mayuri <mayurigaikwad2929@gmail.com>
2025-07-31 12:34:00 +0530

    this is java programme

---
+++ SampleDataBatch/src/com/velocity/Demo.java
@@ -0,0 +1,7 @@
+package com.velocity;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("this is prashant ");
+}
+}
--- SampleDataBatch/src/com/velocity/Example.java
+++ SampleDataBatch/src/com/velocity/Example.java
@@ -1,5 +1,10 @@
 package com.velocity;
 
 public class Example {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
+	}
 
 }
